13141a833ee2a309011a977e8b5d090caa0ed2d6
Saniya Kulkarni <s27kulka@uwaterloo.ca>
2025-03-24 00:44:10 -0400

    Ktor bug fix for NewsBiasApi - api now working

--- project/app/src/main/java/com/example/mynews/data/api/newsbias/NewsBiasApiClient.kt
+++ project/app/src/main/java/com/example/mynews/data/api/newsbias/NewsBiasApiClient.kt
@@ -3,24 +3,72 @@ package com.example.mynews.data.api.newsbias
 import com.example.mynews.data.api.newsbias.NewsBiasResponse
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.request.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.KotlinxSerializationConverter
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.api.Send
+import io.ktor.client.plugins.api.createClientPlugin
+import io.ktor.http.*
+import io.ktor.util.*
+
+// Custom plugin to remove the Accept-Charset header
+val RemoveAcceptCharset = createClientPlugin("RemoveAcceptCharset") {
+    on(Send) { request ->
+        request.headers.remove(HttpHeaders.AcceptCharset)
+        proceed(request)
+    }
+}
+
 
 class NewsBiasApiClient {
 
     private val baseUrl = "https://www.allsides.com/"
     private val userAgent = "Mozilla/5.0"
 
-    private val client = HttpClient {
+    /*private val client = HttpClient {
         install(ContentNegotiation) {
             json(Json {
                 ignoreUnknownKeys = true
                 coerceInputValues = true
             })
         }
+    }*/
+
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            // register both content types
+            json(Json {
+                ignoreUnknownKeys = true
+                coerceInputValues = true
+            }, contentType = ContentType("text", "json"))
+
+            json(Json {
+                ignoreUnknownKeys = true
+                coerceInputValues = true
+            }, contentType = ContentType.Application.Json)
+        }
+
+        install(DefaultRequest) {
+            header(HttpHeaders.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+            header(HttpHeaders.Accept, "application/json, text/javascript, */*; q=0.01")
+            header(HttpHeaders.AcceptLanguage, "en-US,en;q=0.9")
+            header(HttpHeaders.AcceptEncoding, "gzip, deflate, br")
+            header("Referer", "https://www.allsides.com/")
+            header(HttpHeaders.Connection, "keep-alive")
+            header("Upgrade-Insecure-Requests", "1")
+        }
+
+        install(RemoveAcceptCharset)
     }
+
+
+
 
     suspend fun getBiasRatings(): NewsBiasResponse {
         return client.get("${baseUrl}media-bias/json/noncommercial/publications") {
